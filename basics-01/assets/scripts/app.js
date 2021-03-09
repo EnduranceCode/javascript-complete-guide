40da3bf950bb85105c8b3f3cbc4cc7447a0b2076
Ricardo do Canto <endurancecode@gmail.com>
2021-03-09 14:06:54 +0000

    S02C023: Use Numbers and Strings

--- basics-01/assets/scripts/app.js
+++ basics-01/assets/scripts/app.js
@@ -1,5 +1,7 @@
 let currentResult = 0;
 
+let calculationDescription = '((' + currentResult + ' + 10) * 3) / 2 -1';
+
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDescription);
