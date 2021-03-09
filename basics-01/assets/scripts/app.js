2f2bfa8d5e4eedca13bb1310daf6fe0fdf3e3c3c
Ricardo do Canto <endurancecode@gmail.com>
2021-03-09 22:16:11 +0000

    S02C039: Add actions to all buttons

--- basics-01/assets/scripts/app.js
+++ basics-01/assets/scripts/app.js
@@ -5,10 +5,49 @@ let currentResult = defaultResult;
 function getUserNumberInput() {
   return parseInt(userInput.value);
 }
-function add() {
-  const calculationDescription = `${currentResult} + ${getUserNumberInput()}`;
-  currentResult = currentResult + getUserNumberInput();
+
+function createDescriptionAndOutputResult(operator, resultBeforeCalculation, userInputNumber) {
+  const calculationDescription = `${resultBeforeCalculation} ${operator} ${userInputNumber}`;
   outputResult(currentResult, calculationDescription);
 }
 
+function add() {
+  const resultBeforeCalculation = currentResult;
+  const userInputNumber = getUserNumberInput();
+
+  const OPERATOR = '+';
+  currentResult = resultBeforeCalculation + userInputNumber;
+  createDescriptionAndOutputResult(OPERATOR, resultBeforeCalculation, userInputNumber);
+}
+
+function subtract() {
+  const resultBeforeCalculation = currentResult;
+  const userInputNumber = getUserNumberInput();
+
+  const OPERATOR = '-';
+  currentResult = resultBeforeCalculation - userInputNumber;
+  createDescriptionAndOutputResult(OPERATOR, resultBeforeCalculation, userInputNumber);
+}
+
+function multiply() {
+  const resultBeforeCalculation = currentResult;
+  const userInputNumber = getUserNumberInput();
+
+  const OPERATOR = '*';
+  currentResult = resultBeforeCalculation * userInputNumber;
+  createDescriptionAndOutputResult(OPERATOR, resultBeforeCalculation, userInputNumber);
+}
+
+function divide() {
+  const resultBeforeCalculation = currentResult;
+  const userInputNumber = getUserNumberInput();
+
+  const OPERATOR = '/';
+  currentResult = resultBeforeCalculation / userInputNumber;
+  createDescriptionAndOutputResult(OPERATOR, resultBeforeCalculation, userInputNumber);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
