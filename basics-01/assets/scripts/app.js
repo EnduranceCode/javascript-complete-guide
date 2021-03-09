4fe57cb016ffc8fae2d9e3eee9329a2eb48d8245
Ricardo do Canto <endurancecode@gmail.com>
2021-03-09 17:49:10 +0000

    S02C034: Execute function 'Indirectly'

--- basics-01/assets/scripts/app.js
+++ basics-01/assets/scripts/app.js
@@ -1,13 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(number1, number2) {
-  const result = number1 + number2;
-  return result;
+function add() {
+  // eslint-disable-next-line no-undef
+  currentResult = currentResult + userInput.value;
+  // eslint-disable-next-line no-undef
+  outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
-
-let calculationDescription = '';
-
-outputResult(currentResult, calculationDescription);
+// eslint-disable-next-line no-undef
+addBtn.addEventListener('click', add);
