81caacc89a351f0595f7c667b757244fcf3680c3
Ricardo do Canto <endurancecode@gmail.com>
2021-03-09 15:00:15 +0000

    Submit Assignment 01

---
+++ assignment-01/assignment.js
@@ -0,0 +1,18 @@
+/* Step 01 */
+const userInput = 6;
+let result;
+
+/* Step 02 */
+result = 18 + userInput;
+
+/* Step 03 */
+result = result - 2;
+result = result * 6;
+result = result / 3;
+
+/* Step 04 */
+// The value of the variable userInput did not change
+
+/* Step 05 */
+alert('Value of userInput: ' + userInput);
+alert('Value of result: ' + result);
