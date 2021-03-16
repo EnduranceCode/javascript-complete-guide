d8a2cad03c64de0f48ad464ee8eead971d560736
EnduranceCode <endurancecode@gmail.com>
2021-03-16 21:27:53 +0000

    S04C093: Use the Ternary Operator

--- monster-killer/assets/scripts/app.js
+++ monster-killer/assets/scripts/app.js
@@ -34,16 +34,8 @@ function strongAttackHandler() {
 }
 
 function attackMonster(attackMode) {
-  let maxDamage;
-  let logEvent;
-
-  if (attackMode === MODE_ATTACK) {
-    maxDamage = ATTACK_VALUE;
-    logEvent = LOG_EVENT_PLAYER_ATTACK;
-  } else {
-    maxDamage = STRONG_ATTACK_VALUE;
-    logEvent = LOG_EVENT_PLAYER_STRONG_ATTACK;
-  }
+  const maxDamage = attackMode === MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
+  const logEvent = attackMode === MODE_ATTACK ? LOG_EVENT_PLAYER_ATTACK : LOG_EVENT_PLAYER_STRONG_ATTACK;
 
   const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
