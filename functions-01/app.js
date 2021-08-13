b4512e08a3395a2b7704e2a5a8fc9f13c0bb7f5a
Ricardo do Canto <endurancecode@gmail.com>
2021-08-13 10:39:54 +0100

    S06C128: Store functions in variables

--- functions-01/app.js
+++ functions-01/app.js
@@ -1,9 +1,7 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
+const start = function startGame() {
   console.log('Game is starting...');
-}
+};
 
-console.dir(startGame);
-
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', start);
