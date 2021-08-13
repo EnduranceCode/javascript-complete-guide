ac9ab13ee96dbcfa795274e90f3c2d412f83f6a0
Ricardo do Canto <endurancecode@gmail.com>
2021-08-13 10:59:04 +0100

    S06C130: Name anonymous functions

--- functions-01/app.js
+++ functions-01/app.js
@@ -1,5 +1,5 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-startGameBtn.addEventListener('click', function () {
+startGameBtn.addEventListener('click', function startGame() {
   console.log('Game is starting..');
 });
